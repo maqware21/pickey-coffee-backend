d4894b7b935b659b577cfeb92a01be5f8814acb8
Anees Malik <malik.anees96@gmail.com>
2022-09-01 14:07:47 +0500

    add Authentication Api's

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('lname');
             $table->string('phone_number');
             $table->string('email')->unique();
-
+            $table->string('verification_token')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
