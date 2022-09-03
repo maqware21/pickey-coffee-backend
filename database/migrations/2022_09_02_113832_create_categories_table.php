011d3353eeb672fa209f2100171d8a549164d549
Ashir-Bhatti <ashirbhatti98@gmail.com>
2022-09-04 00:59:54 +0500

    Category Crud

--- database/migrations/2022_09_02_113832_create_categories_table.php
+++ database/migrations/2022_09_02_113832_create_categories_table.php
@@ -15,7 +15,7 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained()->onDelete('cascade')->nullable();
+            $table->foreignId('category_id')->nullable();
             $table->string('name');
             $table->string('short_description');
             $table->string('long_description');
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\{ForgotPasswordController, LocationController, LoginController, ProfileController, VerifyEmailController};
+use App\Http\Controllers\{CategoryController, ForgotPasswordController, LocationController, LoginController, ProfileController, VerifyEmailController};
 use GuzzleHttp\Middleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -48,7 +48,10 @@ Route::group(['prefix' => 'profile', 'middleware' => ('auth:sanctum'), 'controll
 	Route::post('change_password', 'change_password');
 });
 
-
-
-// Route::post('update', [ProfileController::class, 'update'])->middleware(['auth:sanctum']);
-// Route::post('change_password', [ProfileController::class, 'change_password'])->middleware(['auth:sanctum']);
+Route::group(['prefix' => 'category', 'middleware' => ('auth:sanctum'), 'controller' => CategoryController::class], function () {
+	
+	Route::post('create', 'create');
+	Route::post('list', 'list');
+	Route::post('update/{id}', 'update');
+	Route::post('delete/{id}', 'delete');
+});
