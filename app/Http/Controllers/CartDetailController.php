ae992c0bdf9c67ce9ff474b3d7483c44111b7046
Anees Malik <malik.anees96@gmail.com>
2022-09-04 14:57:26 +0500

    db structure update

---
+++ app/Http/Controllers/CartDetailController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cart_detail;
+use Illuminate\Http\Request;
+
+class CartDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cart_detail  $cart_detail
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cart_detail $cart_detail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Cart_detail  $cart_detail
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cart_detail $cart_detail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cart_detail  $cart_detail
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cart_detail $cart_detail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cart_detail  $cart_detail
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cart_detail $cart_detail)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_09_04_090824_create_payments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->string('payment_type');
+            $table->string('provider');
+            $table->foreignId('user_id')->constrained();
+            $table->string('account_no');
+            $table->string('expiry');
+            $table->string('cvc_no');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
